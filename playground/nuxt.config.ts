9aa8f72a426aaae4ef5ffce66762b2403cf5fd8f
Jorge Garduño <jorge@rem.tools>
2023-01-06 15:35:53 -0600

    chore: testing new module config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -6,7 +6,12 @@ export default defineNuxtConfig({
     MyModule
   ],
   ory: {
-    server: {
+    custom: {
+      url: 'https://public.test.rem.tools/identities/users/me',
+      transform: ({ result }) => ({
+        session: result.identity,
+        extra: result.company
+      })
     },
     router: {
       // redirectsTo: 'https://account.remtools.io'
